31df1ec49b27921e59fa391bbd5de2f4ce36ff1f
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-11-23 15:52:16 -0300

    added charCount

--- qrGenerator.js
+++ qrGenerator.js
@@ -1,22 +1,50 @@
+// Some hardcoding stuff
+
+QR_VERSION = 13
+QR_ERROR_CORRECTION = "Q"
+
+
 function getEncodingMode(textToEncode){
   regexNumeric = /^\d+$/;
   regexAlphanumeric = /^[\dA-Z $%*+\-./:]*$/;
   regexByte = /^[\x00-\xff]*$/;
   regexKanji = /^[\p{Script_Extensions=Han}\p{Script_Extensions=Hiragana}\p{Script_Extensions=Katakana}]*$/u;
   if (regexNumeric.test(textToEncode)){
-    return 
+    return "0001"
   }
   
   if (regexAlphanumeric.test(textToEncode)){
-    return 
+    return "0010"
   }
   
   if (regexByte.test(textToEncode)){
-    return 
+    return "0100"
   }
   
   if (regexKanji.test(textToEncode)){
-    return 
+    return "1000"
   } 
+  return "0111"
 }
+
+CHAR_COUNT = {
+  "0001": 12,
+  "0010": 11,
+  "0100": 16,
+  "1000": 10
+}
+
+function getCharCount(textLength, mode){
+  binary = textLength.toString(2)
+  
+  return binary.padStart(CHAR_COUNT[mode], "0")
+}
+
+url = "Hello, world!"
+
+console.log(url.charCodeAt(0).toString(2).padStart(8,"0"))
+
+enMode = getEncodingMode(url)
+charCount = getCharCount(url.length, enMode)
+console.log(enMode + charCount)
 
